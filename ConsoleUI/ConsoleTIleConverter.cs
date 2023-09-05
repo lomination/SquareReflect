2d4602fd981ff81e4307630a551ad20e4bb7358b
lomination <lomination1@gmail.com>
2023-09-05 18:11:11 +0200

    Update ConsoleTileConverter (using tile id instead of pattern matching)

--- ConsoleUI/ConsoleTIleConverter.cs
+++ ConsoleUI/ConsoleTIleConverter.cs
@@ -1,23 +1,23 @@
 public static class ConsoleTileConverter {
     public static ConsoleTile ConvertTile(Tile tile) {
-        return tile.GetType().Name switch {
-            nameof(Empty) => new ConsoleTile(tile.Clone(), t => " "),
-            nameof(Block) => new ConsoleTile(tile.Clone(), t => "■"),
-            nameof(Angle) => new ConsoleTile(tile.Clone(), t => "◣◤◥◢"[(int)((Angle)tile).Dir].ToString()),
-            nameof(Start) => new ConsoleTile(tile.Clone(), t => " "),
-            nameof(End) => new ConsoleTile(tile.Clone(), t => "⚑"),
-            nameof(Death) => new ConsoleTile(tile.Clone(), t => "⨯"),
-            nameof(Cannon) => new ConsoleTile(tile.Clone(), t => "⊕"),
-            nameof(Portal) => new ConsoleTile(tile.Clone(), t => ((Portal)tile).PortalId.ToString()),
-            nameof(Blocker) => new ConsoleTile(tile.Clone(), t => "⍓⍄⍌⍃"[(int)((Blocker)tile).Dir].ToString()),
-            nameof(Arrow) => new ConsoleTile(tile.Clone(), t => "⯭⯮⯯⯬"[(int)((Arrow)tile).Dir].ToString()),
-            nameof(Tunnel) => new ConsoleTile(tile.Clone(), t => "▥▤"[(int)((Tunnel)tile).Dir % 2].ToString()),
-            nameof(FragileBlock) => new ConsoleTile(tile.Clone(), t => ((FragileBlock)t).Count > 0 ? "⯀" : "⛶"),
-            nameof(GhostBlock) => new ConsoleTile(tile.Clone(), t => ((GhostBlock)t).Count > 0 ? "⬚" : "⯀"),
-            nameof(FragileAngle) => new ConsoleTile(tile.Clone(), t => ((FragileAngle)t).Count > 0 ? "◣◤◥◢"[(int)((FragileAngle)tile).Dir].ToString() : "◺◸◹◿"[(int)((FragileAngle)tile).Dir].ToString()),
-            nameof(GhostAngle) => new ConsoleTile(tile.Clone(), t => ((GhostAngle)t).Count > 0 ? "◺◸◹◿"[(int)((GhostAngle)tile).Dir].ToString() : "◣◤◥◢"[(int)((GhostAngle)tile).Dir].ToString()),
-            nameof(Key) => new ConsoleTile(tile.Clone(), t => ((Key)t).IsTaken ? " " : "⚿"),
-            nameof(LockBlock) => new ConsoleTile(tile.Clone(), t => ((LockBlock)t).IsLocked ? "🄺" : " "),
+        return tile.GetId() switch {
+            0 => new ConsoleTile(tile.Clone(), t => " "),
+            1 => new ConsoleTile(tile.Clone(), t => "■"),
+            2 => new ConsoleTile(tile.Clone(), t => "◣◤◥◢"[(int)((Angle)tile).Dir].ToString()),
+            3 => new ConsoleTile(tile.Clone(), t => " "),
+            4 => new ConsoleTile(tile.Clone(), t => "⚑"),
+            5=> new ConsoleTile(tile.Clone(), t => "⨯"),
+            6 => new ConsoleTile(tile.Clone(), t => "⊕"),
+            7 => new ConsoleTile(tile.Clone(), t => ((Portal)tile).PortalId.ToString()),
+            8 => new ConsoleTile(tile.Clone(), t => "⍓⍄⍌⍃"[(int)((Blocker)tile).Dir].ToString()),
+            9 => new ConsoleTile(tile.Clone(), t => "⯭⯮⯯⯬"[(int)((Arrow)tile).Dir].ToString()),
+            10 => new ConsoleTile(tile.Clone(), t => "▥▤"[(int)((Tunnel)tile).Dir % 2].ToString()),
+            11 => new ConsoleTile(tile.Clone(), t => ((FragileBlock)t).Count > 0 ? "⯀" : "⛶"),
+            12 => new ConsoleTile(tile.Clone(), t => ((GhostBlock)t).Count > 0 ? "⬚" : "⯀"),
+            13 => new ConsoleTile(tile.Clone(), t => ((FragileAngle)t).Count > 0 ? "◣◤◥◢"[(int)((FragileAngle)tile).Dir].ToString() : "◺◸◹◿"[(int)((FragileAngle)tile).Dir].ToString()),
+            14 => new ConsoleTile(tile.Clone(), t => ((GhostAngle)t).Count > 0 ? "◺◸◹◿"[(int)((GhostAngle)tile).Dir].ToString() : "◣◤◥◢"[(int)((GhostAngle)tile).Dir].ToString()),
+            15 => new ConsoleTile(tile.Clone(), t => ((Key)t).IsTaken ? " " : "⚿"),
+            16 => new ConsoleTile(tile.Clone(), t => ((LockBlock)t).IsLocked ? "🄺" : " "),
             _ => throw new Exception()
         };
     }
